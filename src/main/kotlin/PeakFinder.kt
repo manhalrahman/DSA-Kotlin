c83acf6164ec2fa8996f7a927488f2160763f41a
manhalrahman <mrmanhal99@gmail.com>
2022-07-11 10:51:19 +0530

    edited PeakFinder.kt

--- src/main/kotlin/PeakFinder.kt
+++ src/main/kotlin/PeakFinder.kt
@@ -1,13 +1,44 @@
 fun main() {
+    val arr = (10 downTo 1).toList()
 
 }
 
+
+fun sortedState(num: Int) {
+
+}
 
 fun findPeak(values : List<Int>): Int {
     if(values.isEmpty()) {
         return 0
     }
-    val a = ArrayList<Int>()
 
     return (values.size-1)
+}
+
+fun isSorted(values: List<Int>): Int {
+
+    // reverse sorted = -1
+    // not sorted = 0
+    // right sorted = 1
+    var sortedValue = 1
+    for(i in 1 until values.size-1) {
+        if(values[i] < values[i-1]) {
+            sortedValue = -1
+            break
+        }
+    }
+
+    if(sortedValue != 1) {
+        for(i in values.size-1 downTo 1) {
+            if (values[i] > values[i - 1]) {
+                sortedValue = 0
+                break
+            }
+        }
+    }
+
+    return sortedValue
+
+
 }
